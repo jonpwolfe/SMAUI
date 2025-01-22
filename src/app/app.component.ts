98ce1d2994e8ec7a8b846aa2a4199e62b44e1e0a
SICARIOXSICARIO <jwolfe27314@gmail.com>
2025-01-22 00:09:09 -0700

    better fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';  // For routing
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';  // For Reactive Forms
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, DashboardComponent, ReactiveFormsModule],
+  standalone: true,  // Standalone component
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SMAUI';
+  title = 'Angular Standalone App';
 }
